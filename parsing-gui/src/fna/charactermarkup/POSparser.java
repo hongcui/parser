ab9f3544407ebbd95ee7e7a6cf9c9eaaf8989fe8
ramu1787 <ramu1787@gmail.com>
2010-06-22 21:49:25 +0000

    Added code to randomly extract 5% of relations and store it in a file

--- parsing-gui/src/fna/charactermarkup/POSparser.java
+++ parsing-gui/src/fna/charactermarkup/POSparser.java
@@ -9,6 +9,13 @@ import java.sql.ResultSet;
 import java.util.*;
 import java.io.File;
 import java.util.regex.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.trees.tregex.*;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
@@ -22,6 +29,7 @@ public class POSparser {
 	static protected String database = null;
 	static protected String username = "root";
 	static protected String password = "";
+	protected int count = 0;
 	private ProcessListener listener;
 
 	public POSparser() {
@@ -58,7 +66,7 @@ public class POSparser {
 			Statement stmt1 = conn.createStatement();
 			Statement stmt2 = conn.createStatement();
 			Statement stmt3 = conn.createStatement();
-			FileOutputStream ostream = new FileOutputStream("F:\\UA\\RA\\Stanford-Parser\\stanford-parser-2010-02-26\\seginput.txt"); 
+			FileOutputStream ostream = new FileOutputStream("F:\\UA\\RA\\Code\\parsing-gui-charactermarkup\\seginput.txt"); 
 			PrintStream out = new PrintStream( ostream );
 			String str;
 			ResultSet rs = stmt.executeQuery("select * from newsegments");
@@ -127,7 +135,9 @@ public class POSparser {
         	
         	Runtime r = Runtime.getRuntime();
 			Process p = r.exec("cmd /c lexparserPOS.bat");
-        	
+        	*/
+			
+			/*
         	FileInputStream istream = new FileInputStream("F:\\UA\\RA\\Code\\parsing-gui-charactermarkup\\finaloutput.txt"); 
 			BufferedReader stdInput = new BufferedReader(new InputStreamReader(istream));
         	String s = "";
@@ -149,39 +159,89 @@ public class POSparser {
 			
 			FileInputStream istream = new FileInputStream("F:\\UA\\RA\\Code\\parsing-gui-charactermarkup\\finaloutput.txt"); 
 			BufferedReader stdInput = new BufferedReader(new InputStreamReader(istream));
+			FileOutputStream ostream = new FileOutputStream("F:\\UA\\RA\\Code\\parsing-gui-charactermarkup\\relationanalysis.txt"); 
+			PrintWriter out = new PrintWriter(ostream);
         	String s = "";
         	String str = "";
-        	int ct = 0;
+        	int sentidct = 0;
+        	int printcount = 0;
 			while ((s=stdInput.readLine())!=null){
 				//System.out.println(s);
 				if(!s.contains("(ROOT")){
 					str = str.concat(s);
 				}					
 				else{
-					ct++;
-					if(ct!=1){
-						System.out.println(ct);
-						System.out.println(str);
-						extractrelation(str);
+					sentidct++;
+					if(sentidct!=1){
+						//System.out.println(str);
+						Random r = new Random();
+						if(r.nextDouble()<.05){
+							String result = extractrelation(str);
+							printcount++;
+							out.println("Sentid: "+(sentidct-1));
+							Pattern pattern2 = Pattern.compile("[\\s]+");
+					        Matcher matcher2 = pattern2.matcher(str);
+					        str = matcher2.replaceAll(" ");
+					        matcher2.reset();
+							out.println("Sentence: "+str);
+					    	out.println("Relation: "+result);
+						}
 						str = "";
 					}
 					//System.out.println(s);
 				}
         	}
-			
+			String result = extractrelation(str);
+			if(result!=""){
+				printcount++;
+				out.println(sentidct-1);
+				out.println(str);
+		    	out.println(result);
+			}
+			//System.out.println(str);
+			System.out.println(printcount);
+			System.out.println(count);
 		}catch (Exception e){
     		//System.err.println(e);
 			e.printStackTrace();
         }
 	}
 	
-	protected void extractrelation (String str) {
+	private Element xmlRoot(String response){
+		Document doc =null;
+		try {
+		     DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		     DocumentBuilder db = dbf.newDocumentBuilder();
+		     ByteArrayInputStream bais = new ByteArrayInputStream(response.getBytes());
+		     doc = db.parse(bais);
+		    } catch (Exception e) {
+		      System.out.print("Problem parsing the xml: \n" + e.toString());
+		}
+		    
+		return doc.getDocumentElement();
+		
+	}
+	
+	protected String extractrelation (String str) {
+		String relation="";
+		String result="";
 		Pattern pattern = Pattern.compile("\\(VP[()\\w\\s]+\\(PP[\\s]\\([\\w\\s]+\\)");
         Matcher matcher = pattern.matcher(str);
         while(matcher.find()){
-        	System.out.println(str.substring(matcher.start(), matcher.end()));
+			count++;
+        	relation = str.substring(matcher.start(), matcher.end());
+        	Pattern pattern1 = Pattern.compile("(\\(|\\)|VP|ADJP|RB|NP|PP|ADVP|CC|CD|JJ|TO[\\s]|IN[\\s]|VB(.?)|NN(.?)|DT)");
+            Matcher matcher1 = pattern1.matcher(relation);
+            relation = matcher1.replaceAll("");
+            matcher1.reset();
+            Pattern pattern2 = Pattern.compile("[\\s]+");
+            Matcher matcher2 = pattern2.matcher(relation);
+            relation = matcher2.replaceAll(" ");
+            result = result.concat(relation+"\n");
+            matcher2.reset();
         }
         matcher.reset();
+        return result;
 	}
 
 	protected String reversecondense(String str) { 
