7f0d22dd55366033df8eecac3cfc62705a035171
ramu1787@gmail.com <ramu1787@gmail.com@e52d38a4-b9ad-11de-b017-a9a2c7051f87>
2010-06-22 21:41:06 +0000

    Eliminating the sentences with <Z> tags and adding organs except explicit ones from sentence table to the markedsentence.

--- trunk/parser/parsing-gui/src/fna/charactermarkup/MarkedSentGenerator.java
+++ trunk/parser/parsing-gui/src/fna/charactermarkup/MarkedSentGenerator.java
@@ -49,43 +49,95 @@ public class MarkedSentGenerator {
         	ResultSet rs = stmt.executeQuery("select * from sentence");
         	while(rs.next()){
         		str = rs.getString(3);
-        		Pattern pattern = Pattern.compile("<N>");
-                Matcher matcher = pattern.matcher(str);
-                str = matcher.replaceAll("<");
-                matcher.reset();
-                Pattern pattern1 = Pattern.compile("</N>");
-                matcher = pattern1.matcher(str);
-                str = matcher.replaceAll(">");
-                matcher.reset();
-                StringBuffer sb = new StringBuffer();
-                Pattern pattern2 = Pattern.compile("(<B>)([\\(\\)?\\–\\—°²:=/½\"¼x´\\×\\*µ%“”+\\d±.;,\\[\\]]+)(</B>)");
-                matcher = pattern2.matcher(str);
-                while (matcher.find()){
-           			matcher.appendReplacement(sb, matcher.group(2));
-                }
-                matcher.appendTail(sb);
-        		str=sb.toString();
-                matcher.reset();
-                StringBuffer sb1 = new StringBuffer();
-                Pattern pattern5 = Pattern.compile("(<B>)([\\w]+)(</B>)");
-                matcher = pattern5.matcher(str);
-                while (matcher.find()){
-                	ResultSet rs1=stmt2.executeQuery("Select * from preposition where prep='"+matcher.group(2)+"'");
-             		if(rs1.next())
-             			matcher.appendReplacement(sb1, matcher.group(2));
-                }
-                matcher.appendTail(sb1);
-        		str=sb1.toString();
-                matcher.reset();
-                Pattern pattern3 = Pattern.compile("<M><B>|<B>|<M>");
-                matcher = pattern3.matcher(str);
-                str = matcher.replaceAll("{");
-                matcher.reset();
-                Pattern pattern4 = Pattern.compile("</B></M>|</B>|</M>");
-                matcher = pattern4.matcher(str);
-                str = matcher.replaceAll("}");
-                matcher.reset();
-        		stmt1.execute("insert into markedsentence values('"+rs.getString(2)+"','"+str+"')");
+        		if(!str.contains("<Z>")){
+	        		Pattern pattern = Pattern.compile("<N>");
+	                Matcher matcher = pattern.matcher(str);
+	                str = matcher.replaceAll("<");
+	                matcher.reset();
+	                Pattern pattern1 = Pattern.compile("</N>");
+	                matcher = pattern1.matcher(str);
+	                str = matcher.replaceAll(">");
+	                matcher.reset();
+	                StringBuffer sb = new StringBuffer();
+	                Pattern pattern2 = Pattern.compile("(<B>)([\\(\\)?\\–\\—°²:=/½\"¼x´\\×\\*µ%“”+\\d±.;,\\[\\]]+)(</B>)");
+	                matcher = pattern2.matcher(str);
+	                while (matcher.find()){
+	           			matcher.appendReplacement(sb, matcher.group(2));
+	                }
+	                matcher.appendTail(sb);
+	        		str=sb.toString();
+	                matcher.reset();
+	                StringBuffer sb1 = new StringBuffer();
+	                Pattern pattern5 = Pattern.compile("(<B>)([\\w]+)(</B>)");
+	                matcher = pattern5.matcher(str);
+	                while (matcher.find()){
+	                	ResultSet rs1=stmt2.executeQuery("Select * from preposition where prep='"+matcher.group(2)+"'");
+	             		if(rs1.next())
+	             			matcher.appendReplacement(sb1, matcher.group(2));
+	                }
+	                matcher.appendTail(sb1);
+	        		str=sb1.toString();
+	                matcher.reset();
+	                Pattern pattern3 = Pattern.compile("<M><B>|<B>|<M>");
+	                matcher = pattern3.matcher(str);
+	                str = matcher.replaceAll("{");
+	                matcher.reset();
+	                Pattern pattern4 = Pattern.compile("</B></M>|</B>|</M>");
+	                matcher = pattern4.matcher(str);
+	                str = matcher.replaceAll("}");
+	                matcher.reset();
+	                
+	                int ct = 0;
+	                if(rs.getString("tag").contains("[")|rs.getString("tag").compareTo("ditto")==0|rs.getString("tag").compareTo("general")==0){
+	                	if(rs.getString("modifier").compareTo("")!=0){
+	                		String tag = rs.getString("tag");
+	                		String modifier = rs.getString("modifier");
+	                		while(tag.compareTo("ditto")==0){
+	                			ct++;
+	                			rs.previous();
+	                			tag = rs.getString("tag");
+	                		}
+	                		if(tag.compareTo("ignore")!=0){
+		                		if(tag.contains("["))
+		                			tag = tag.substring(1, tag.length()-1);
+		                		if(modifier.contains(" ")){
+		            				String [] terms = modifier.split(" ");
+		            				modifier = terms[terms.length-1];
+		            				if(modifier.contains("["))
+		            					modifier = terms[terms.length-2];
+		            			}
+		                		StringBuffer sb2 = new StringBuffer();
+		    	                Pattern pattern6 = Pattern.compile("\\{"+modifier+"\\}");
+		    	                matcher = pattern6.matcher(str);
+		    	                while (matcher.find()){
+		    	           			matcher.appendReplacement(sb2, matcher.group(0)+" <"+tag+">");
+		    	                }
+		    	                matcher.appendTail(sb2);
+		    	        		str=sb2.toString();
+		    	                matcher.reset();
+	                		}
+	                	}
+	                	else{
+	                		String tag = rs.getString("tag");
+	                		while(tag.compareTo("ditto")==0){
+	                			ct++;
+	                			rs.previous();
+	                			tag = rs.getString("tag");
+	                		}
+	                		if(tag.compareTo("ignore")!=0){
+		                		if(tag.contains("["))
+		                			tag = tag.substring(1, tag.length()-1);
+		                		str = "<"+tag+"> "+str;
+	                		}
+	                	}
+	                }
+	                if(ct>0){
+	                	for(int i = 0; i < ct; i++)
+	                		rs.next();
+	                }
+	                
+	        		stmt1.execute("insert into markedsentence values('"+rs.getString(2)+"','"+str+"')");
+        		}
         	}
 		}catch (Exception e)
         {
@@ -99,7 +151,7 @@ public class MarkedSentGenerator {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new MarkedSentGenerator("benchmark_learningcurve_fnav19_test_24");
+		new MarkedSentGenerator("benchmark_learningcurve_treatiseh_test_19");
 	}
 
 }
