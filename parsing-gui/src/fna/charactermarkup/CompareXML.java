86832de8b7c3b69c0e2dfef4b84917e32927558b
ramu1787 <ramu1787@gmail.com>
2010-06-22 21:33:06 +0000

    Performed some corrections on validatecharacter.

--- parsing-gui/src/fna/charactermarkup/CompareXML.java
+++ parsing-gui/src/fna/charactermarkup/CompareXML.java
@@ -1,6 +1,8 @@
 package fna.charactermarkup;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,6 +13,7 @@ import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
+import org.jdom.filter.ElementFilter;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
@@ -37,14 +40,35 @@ public class CompareXML {
 	public CompareXML() {
 		try{
 		// TODO Auto-generated constructor stub
-			SAXBuilder builder = new SAXBuilder();
-			Document anskey = builder.build("F:\\UA\\RA\\Code\\parsing-gui-charactermarkup\\anskey.xml");
-			Element ansroot = anskey.getRootElement();
-			Document testcase = builder.build("F:\\UA\\RA\\Code\\parsing-gui-charactermarkup\\testcase.xml");
-			Element testroot = testcase.getRootElement();
-			validatestruct(ansroot, testroot);
-			validatecharacter(ansroot, testroot);
-			validaterelation(ansroot, testroot);
+			File ansdirectory = new File("F:\\UA\\RA\\AnsKey_Benchmark");
+			String ansfilename[] = ansdirectory.list();
+			File testdirectory = new File("F:\\UA\\RA\\TestCase_Benchmark");
+			String testfilename[] = testdirectory.list();
+			for (int i = 0; i < ansfilename.length; i++) {
+				for (int j = 0; j < testfilename.length; j++) {
+					if(ansfilename[i].compareTo(testfilename[j])==0){
+						SAXBuilder builder = new SAXBuilder();
+						Document anskey = builder.build("F:\\UA\\RA\\AnsKey_Benchmark\\"+ansfilename[i]);
+						Element ansroot = anskey.getRootElement();
+						Document testcase = builder.build("F:\\UA\\RA\\TestCase_Benchmark\\"+testfilename[j]);
+						Element testroot = testcase.getRootElement();
+						System.out.println("File: "+ansfilename[i]);
+						structexactmatch = 0;
+						structpartmatch = 0;
+						structnomatch = 0;
+						charexactmatch = 0;
+						charpartmatch = 0;
+						charnomatch = 0;
+						relexactmatch = 0;
+						relpartmatch = 0;
+						relnomatch = 0;
+						validatestruct(ansroot, testroot);
+						validatecharacter(ansroot, testroot);
+						validaterelation(ansroot, testroot);
+						break;
+					}
+				}
+			}
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -135,113 +159,132 @@ public class CompareXML {
 		
 	private void validatecharacter(Element ansroot, Element testroot) {
 		String exact = "";
+		String ansliexact = "";
+		ArrayList<List> myansli = new ArrayList();
+		ArrayList<List> mytestli = new ArrayList();
+		List ansli = null, testli = null;
 		List structansli = ansroot.getChildren("structure");
 		List structtestli = testroot.getChildren("structure");
-		List ansli = null, testli = null;
 		int flags = 0;
 		for( Iterator l = structansli.iterator(); l.hasNext();){
 			Element structans = (Element)l.next();
 			if(!structans.getChildren().isEmpty()){
-				if(flags == 0){
 					ansli = structans.getChildren();
-					flags = 1;
-				}
-				else
-					ansli.addAll(structans.getChildren());
+					myansli.add(ansli);
 			}
 		}
 		flags = 0;
 		for( Iterator l = structtestli.iterator(); l.hasNext();){
 			Element structtest = (Element)l.next();
 			if(!structtest.getChildren().isEmpty()){
-				if(flags == 0){
 					testli = structtest.getChildren();
-					flags = 1;
-				}
-				else
-					testli.addAll(structtest.getChildren());
+					mytestli.add(testli);
 			}
 		}
-		//System.out.println(ansli.size());
-		//System.out.println(testli.size());
-		for(int i = 0; i < testli.size(); i++){
-			int flag = 0;
-			Element test = (Element)testli.get(i);
-			for(int j = 0; j < ansli.size(); j++){
-				flag = 0;
-				Element ans = (Element)ansli.get(j);
-				//System.out.println(test.getParentElement().getAttributeValue("id"));
-				//System.out.println(ans.getParentElement().getAttributeValue("id"));
-				if (test.getParentElement().getAttributeValue("id").compareTo(ans.getParentElement().getAttributeValue("id"))==0){
-					List testattr = test.getAttributes();
-					List ansattr = ans.getAttributes();
-					if (testattr.size() == ansattr.size()){
-						for(Iterator k = testattr.iterator(); k.hasNext();){
-							Attribute a = (Attribute)k.next();
-							//System.out.println(ansattr.toString());
-							//System.out.println(a.toString());
-							if(!ansattr.toString().contains(a.toString())){
-								flag = 1;
-								break;
-							}
-						}
-						if(flag == 0){
-							charexactmatch++;
-							exact += test.getAttributes().toString();
-							ansli.remove(j);
-							break;
-						}
-					}
-				}
-			}
-		}
-		
-		for(int i = 0; i < testli.size(); i++){
-			int flag = 0;
-			Element test = (Element)testli.get(i);
-			if(!exact.contains(test.getAttributes().toString())){
-				for(int j = 0; j < ansli.size(); j++){
-					Element ans = (Element)ansli.get(j);
-					if (test.getParentElement().getAttributeValue("id").compareTo(ans.getParentElement().getAttributeValue("id"))==0){
-						int count = 0, passct = 0;
-						List testattr = test.getAttributes();
-						List ansattr = ans.getAttributes();
-						for(int k = 0; k < testattr.size(); k++){
-							Attribute atest = (Attribute)testattr.get(k);
-							for(int l = 0; l < ansattr.size(); l++){
-								Attribute aans = (Attribute)ansattr.get(l);
-								if(atest.getName().toString().compareTo(aans.getName().toString())==0){
-									passct++;
-									if(!aans.getValue().contains(atest.getValue())|!atest.getValue().contains(aans.getValue())){
-										count++;
+
+		if(myansli!=null && mytestli!=null){
+			for(Iterator tli = mytestli.iterator(); tli.hasNext();){
+				testli = (List)tli.next();
+				for(int i = 0; i < testli.size(); i++){
+					int flag = 0;
+					Element test = (Element)testli.get(i);
+					for(Iterator ali = myansli.iterator(); ali.hasNext();){
+						int aliflag = 0;
+						ansli = (List)ali.next();
+						for(int j = 0; j < ansli.size(); j++){
+							flag = 0;
+							Element ans = (Element)ansli.get(j);
+							//System.out.println(test.getParentElement().getAttributeValue("id"));
+							//System.out.println(ans.getParentElement().getAttributeValue("id"));
+							if (test.getParentElement().getAttributeValue("id").compareTo(ans.getParentElement().getAttributeValue("id"))==0){
+								List testattr = test.getAttributes();
+								List ansattr = ans.getAttributes();
+								if (testattr.size() == ansattr.size()){
+									for(Iterator k = testattr.iterator(); k.hasNext();){
+										Attribute a = (Attribute)k.next();
+										//System.out.println(ansattr.toString());
+										//System.out.println(a.toString());
+										if(!ansattr.toString().contains(a.toString())){
+											flag = 1;
+											break;
+										}
+									}
+									if(flag == 0){
+										charexactmatch++;
+										exact += test.getAttributes().toString();
+										ansliexact += ans.getAttributes().toString();
+										aliflag = 1;
 										break;
 									}
-									else
-										break;
 								}
 							}
 						}
-						if(count == 1 && passct!=1){
-							charpartmatch++;
-							flag = 1;
+						if(aliflag == 1)
 							break;
+					}
+				}
+			}
+			
+			for(Iterator tli = mytestli.iterator(); tli.hasNext();){
+				testli = (List)tli.next();			
+				for(int i = 0; i < testli.size(); i++){
+					int flag = 0;
+					Element test = (Element)testli.get(i);
+					if(!exact.contains(test.getAttributes().toString())){
+						for(Iterator ali = myansli.iterator(); ali.hasNext();){
+							int aliflag = 0;
+							ansli = (List)ali.next();
+							for(int j = 0; j < ansli.size(); j++){
+								Element ans = (Element)ansli.get(j);
+								if(!ansliexact.contains(ans.getAttributes().toString())){
+									if (test.getParentElement().getAttributeValue("id").compareTo(ans.getParentElement().getAttributeValue("id"))==0){
+										int count = 0, passct = 0;
+										List testattr = test.getAttributes();
+										List ansattr = ans.getAttributes();
+										for(int k = 0; k < testattr.size(); k++){
+											Attribute atest = (Attribute)testattr.get(k);
+											for(int l = 0; l < ansattr.size(); l++){
+												Attribute aans = (Attribute)ansattr.get(l);
+												if(atest.getName().toString().compareTo(aans.getName().toString())==0){
+													passct++;
+													if(!aans.getValue().contains(atest.getValue())|!atest.getValue().contains(aans.getValue())){
+														count++;
+														break;
+													}
+													else
+														break;
+												}
+											}
+										}
+										if(count == 1 && passct!=1){
+											charpartmatch++;
+											flag = 1;
+											aliflag = 1;
+											break;
+										}
+										else if(count == 0 && (passct==testattr.size()|passct==testattr.size()-1)){
+											charpartmatch++;
+											flag = 1;
+											aliflag = 1;
+											break;
+										}
+									}
+								}
+							}
+							if (aliflag == 1){
+								break;
+							}
 						}
-						else if(count == 0 && (passct==testattr.size()|passct==testattr.size()-1)){
-							charpartmatch++;
-							flag = 1;
-							break;
+						if (flag == 0){
+							charnomatch++;
 						}
 					}
 				}
-				if (flag == 0){
-					charnomatch++;
-				}
 			}
+			System.out.println("charexactmatch:"+charexactmatch);
+			System.out.println("charpartmatch:"+charpartmatch);
+			System.out.println("charnomatch:"+charnomatch);
 		}
-		System.out.println("charexactmatch:"+charexactmatch);
-		System.out.println("charpartmatch:"+charpartmatch);
-		System.out.println("charnomatch:"+charnomatch);
-		
 	}
 	
 	private void validaterelation(Element ansroot, Element testroot) {
