1b54654680567315f8be023056212beb8ad35d29
hong1.cui <hong1.cui@e52d38a4-b9ad-11de-b017-a9a2c7051f87>
2012-11-28 20:44:58 +0000

    Prep 4 Dec Workshop

--- Unsupervised/test.pl
+++ Unsupervised/test.pl
@@ -6,10 +6,21 @@ use strict;
 
 #print $pos;
 
-my $sent = "hong's mom";
-$sent =~ s#'#\\'#g;
-print $sent; #escape
+#my $sent = "hong's mom";
+#$sent =~ s#'#\\'#g;
+#print $sent; #escape
 
 #$dbh->prepare("select * from anytable where avalue in ('a', 'b', 'c')");
+
+my @words = ([1, 2, 3], [4, 5, 6]);
+
+for(my $i = 0; $i <= $#words; $i++){
+	for(my $j = 0; $j <=$#{$words[$i]}; $j++){	
+		print "here $i, $j , $words[$i][$j]\n";
+	}
+}
+
+
+
 
 
