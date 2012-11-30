fc9f0f604404d3bce93a9f7ce8fa0b62cc450852
hong1.cui <hong1.cui@e52d38a4-b9ad-11de-b017-a9a2c7051f87>
2012-11-30 00:46:54 +0000

    Prep 4 Dec Workshop

--- trunk/parser/parsing-gui/src/fna/parsing/FileName2Taxon.java
+++ trunk/parser/parsing-gui/src/fna/parsing/FileName2Taxon.java
@@ -31,21 +31,7 @@ public abstract class FileName2Taxon {
 	 * 
 	 */
 	public FileName2Taxon(String inputfilepath, String database, String prefix) {
-		values.put("filename", "");
-		values.put("hasdescription", "");
-		values.put("family", "");
-		values.put("subfamily", "");
-		values.put("tribe", "");
-		values.put("subtribe", "");
-		values.put("genus", "");
-		values.put("subgenus", "");
-		values.put("section", "");
-		values.put("subsection", "");
-		values.put("species", "");
-		values.put("subspecies", "");
-		values.put("variety", "");
-		
-		
+		resetValues();		
 		this.inputfilepath = inputfilepath;
 		this.database = database;
 		this.dataprefix = prefix;
@@ -61,6 +47,36 @@ public abstract class FileName2Taxon {
 		}
 	}
 
+	protected void resetValues() {
+		values.put("filename", "");
+		values.put("hasdescription", "");
+		values.put("domain", "");
+		values.put("kingdom", "");
+		values.put("phylum", "");
+		values.put("subphylum", "");
+		values.put("superdivision", "");
+		values.put("division", "");
+		values.put("subdivision", "");
+		values.put("superclass", "");
+		values.put("class", "");
+		values.put("subclass", "");
+		values.put("superorder", "");
+		values.put("order", "");
+		values.put("suborder", "");
+		values.put("superfamily", "");
+		values.put("family", "");
+		values.put("subfamily", "");
+		values.put("tribe", "");
+		values.put("subtribe", "");
+		values.put("genus", "");
+		values.put("subgenus", "");
+		values.put("section", "");
+		values.put("subsection", "");
+		values.put("species", "");
+		values.put("subspecies", "");
+		values.put("variety", "");
+	}
+
 	protected void populateFilename2TaxonTable(){
 		File[] xmls = (new File(this.inputfilepath)).listFiles();
 		int size = xmls.length;
@@ -73,8 +89,25 @@ public abstract class FileName2Taxon {
 	
 	protected abstract void populateFilename2TaxonTableUsing(File xml);
 	public void insertIntoFilename2TaxonTable(){
+		
 		String filename = values.get("filename");
 		int hasdescription = Integer.parseInt(values.get("hasdescription"));
+		String domain = values.get("domain");
+		String kingdom = values.get("kingdom");
+		String phylum = values.get("phylum");
+		String subphylum = values.get("subphylum");
+		String superdivision = values.get("superdivision");
+		String division = values.get("division");
+		String subdivision = values.get("subdivision");
+		String superclass = values.get("superclass");
+		String classs = values.get("class");
+		String subclass = values.get("subclass");
+		String superorder = values.get("superorder");
+		String order = values.get("order");
+		String suborder = values.get("suborder");
+		String superfamily = values.get("superfamily");
+		
+		
 		String family = values.get("family");
 		String subfamily = values.get("subfamily");
 		String tribe = values.get("tribe");
@@ -90,7 +123,10 @@ public abstract class FileName2Taxon {
 		try{
 			Statement stmt = conn.createStatement();
 			stmt.execute("insert into "+dataprefix+"_filename2taxon values (" +
-					"'"+filename+"'," +hasdescription+",'" +family+"','" +subfamily+"','" 
+					"'"+filename+"'," +hasdescription+",'" +domain+"','"+kingdom+"','"+phylum+"','"+
+					subphylum+"','"+superdivision+"','"+division+"','"+subdivision+"','"+superclass+
+					"','"+classs+"','"+subclass+"','"+superorder+"','"+order+"','"+suborder+"','"
+					+superfamily+"','"+family+"','" +subfamily+"','" 
 					+tribe+"','" +subtribe+"','" + genus+"','" +subgenus+"','" +section+"','" +subsection+"','" 
 					+species+"','" +subspecies+"','" +variety+"')");
 
@@ -108,6 +144,20 @@ public abstract class FileName2Taxon {
 			stmt.execute("create table if not exists "+dataprefix+"_filename2taxon (" +
 					"`filename` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL," +
 					"`hasdescription` tinyint(1) DEFAULT 0," +
+					"`domain` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`kingdom` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`phylum` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`subphylum` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`superdivision` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`division` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`subdivision` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`superclass` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`class` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`subclass` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`superorder` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`order` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`suborder` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
+					"`superfamily` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
 					"`family` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin," +
 					"`subfamily` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin ," +
 					"`tribe` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin ," +
---
+++ trunk/parser/parsing-gui/src/fna/parsing/FileName2TaxonTreatise.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package fna.parsing;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Hashtable;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.xpath.XPath;
+
+/**
+ * @author Hong Updates
+ *
+ */
+public class FileName2TaxonTreatise extends FileName2Taxon {
+	
+	static	XPath namepath;
+	static	XPath rankpath;
+	static	XPath hierarchypath;
+    static XPath descriptionpath;
+	static{
+		try{
+			namepath = XPath.newInstance("//name");
+			rankpath = XPath.newInstance("//rank");
+			hierarchypath = XPath.newInstance("//taxon_hierarchy");
+			descriptionpath = XPath.newInstance("//description");
+		}catch(Exception e){
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			LOGGER.error(ApplicationUtilities.getProperty("CharaParser.version")+System.getProperty("line.separator")+sw.toString());
+		}
+	}
+
+	/**
+	 * @param inputfilepath
+	 * @param database
+	 * @param prefix
+	 */
+	public FileName2TaxonTreatise(String inputfilepath, String database,
+			String prefix) {
+		super(inputfilepath, database, prefix);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see fna.parsing.FileName2Taxon#populateFilename2TaxonTableUsing(java.io.File)
+	 */
+	@Override
+	/**
+	 * <nomenclature>
+	 * 	<name>Lunolenus SDZUY</name>
+	 * 	<rank>Genus</rank>
+	 * 	<taxon_hierarchy>Class TRILOBITA Walch; Order REDLICHIIDA Richter; Suborder REDLICHIINA Richter; Superfamily REDLICHIOIDEA Poulsen; Family ABADIELLIDAE Hupé</taxon_hierarchy>
+	 * 	<name_info>Lunolenus SDZUY, 1961, p. 549 [*L. lunae; OD; holo-type (SDZUY, 1961, pl. 7, fig. 9), L3110, UMU, Münster].</name_info>
+	 * </nomenclature>
+	 * use <name>, <rank>, and <taxon_hierarchy> to populate values hashtable
+	 */
+	protected void populateFilename2TaxonTableUsing(File xml) {
+		try{
+			SAXBuilder builder = new SAXBuilder();
+			Document doc = builder.build(xml);
+			Element root = doc.getRootElement();
+			if(descriptionpath.selectNodes(root).size() > 0){
+				values.put("hasdescription", "1");
+			}else{
+				values.put("hasdescription", "0");
+			}
+			values.put("filename", xml.getName());
+			String aname = ((Element)namepath.selectSingleNode(root)).getTextNormalize().toLowerCase();
+			if(aname.contains(" ")){
+				String temp = aname.substring(0, aname.indexOf(" "));
+				if(temp.matches("[a-z]\\.")){//get the next word : O. (Oncagnostus)
+					aname = aname.substring(3).replaceAll("[()]", "").trim();
+					if(aname.contains(" ")) aname = aname.substring(0, aname.indexOf(" "));
+				}else if(values.get(temp) !=null){//is rank, get the next word
+					aname = aname.substring(aname.indexOf(" ")).trim(); //skip rank
+					if(aname.contains(" ")) aname = aname.substring(0, aname.indexOf(" "));
+				}else if(aname.contains(" ")){
+					aname = aname.substring(0, aname.indexOf(" "));
+				}
+			}
+			
+			String rank = ((Element)rankpath.selectSingleNode(root)).getTextNormalize().toLowerCase();
+			values.put(rank, aname);
+			
+			String hierarchy = ((Element)hierarchypath.selectSingleNode(root)).getTextNormalize().toLowerCase();
+			String[] names = hierarchy.split("\\s*;\\s*");
+			for(String name: names){
+				String rnk = name.substring(0, name.indexOf(" "));
+				if(values.get(rnk)==null){
+					rnk = "genus"; //rank Genus was not used in the original text, add it 
+					name = name.replaceFirst(rnk, "").trim();
+					if(name.contains(" ")) name = name.substring(0, name.indexOf(" "));
+					values.put(rnk, name);
+					break; //Genus is the lowest rank.
+				}
+				name = name.replaceFirst(rnk, "").trim();
+				if(name.contains(" ")) name = name.substring(0, name.indexOf(" "));
+				values.put(rnk, name);
+			}			
+			insertIntoFilename2TaxonTable();
+			resetValues();
+		}catch(Exception e){
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			LOGGER.error(ApplicationUtilities.getProperty("CharaParser.version")+System.getProperty("line.separator")+sw.toString());
+		}		
+	}
+		
+
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String filepath = "C:\\Documents and Settings\\Hong Updates\\Desktop\\CharaParserWorkshop2012\\TreatisePartO\\source";
+		String database = "markedupdatasets";
+		String prefix = "treatise_o";
+		FileName2TaxonTreatise fntf = new FileName2TaxonTreatise(filepath, database, prefix);
+		fntf.createFilename2taxonTable();
+		fntf.populateFilename2TaxonTable();
+	}
+
+}
